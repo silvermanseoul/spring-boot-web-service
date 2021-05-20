5121b42c4b6eb33a72c11a110f12bad25f736b83
silvermanseoul <silverman0822@gmail.com>
2021-05-20 23:58:15 +0900

    Initial commit

---
+++ src/test/java/io/silverman/springbootwebservice/SpringBootWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.silverman.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
