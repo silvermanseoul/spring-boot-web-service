d993495d2fdac0bd06591f6e443fec0e6d6b1c46
silvermanseoul <silverman0822@gmail.com>
2021-06-08 23:59:08 +0900

    feat: PostApiController 추가

--- src/main/java/io/silverman/springbootwebservice/web/PostApiController.java
+++ src/main/java/io/silverman/springbootwebservice/web/PostApiController.java
@@ -1,2 +1,20 @@
-package io.silverman.springbootwebservice.web;public class PostApiController {
+package io.silverman.springbootwebservice.web;
+
+import io.silverman.springbootwebservice.service.post.PostService;
+import io.silverman.springbootwebservice.web.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostSaveRequestDto requestDto) {
+        return postService.save(requestDto);
+    }
 }
