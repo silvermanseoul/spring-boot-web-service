834d66f20de09f520507a567fb37b3816e92e793
silvermanseoul <silverman0822@gmail.com>
2021-06-22 23:55:21 +0900

    test: PostRepositoryTest에 BaseTimeEntity 테스트 추가

--- src/test/java/io/silverman/springbootwebservice/domain/post/PostRepositoryTest.java
+++ src/test/java/io/silverman/springbootwebservice/domain/post/PostRepositoryTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PostRepositoryTest {
@@ -40,5 +40,22 @@ class PostRepositoryTest {
         assertEquals(title, post.getTitle());
         assertEquals(content, post.getContent());
         assertNull(post.getAuthor());
+    }
+
+    @Test
+    void BaseTimeEntity_저장_조회() {
+        // Given
+        postRepository.save(Post.builder()
+                .title("테스트 제목")
+                .content("테스트 본문")
+                .build());
+
+        // When
+        List<Post> posts = postRepository.findAll();
+
+        // Then
+        Post post = posts.get(0);
+        assertTrue(post.getCreatedDate().isBefore(LocalDateTime.now()));
+        assertTrue(post.getModifiedDate().isBefore(LocalDateTime.now()));
     }
 }
