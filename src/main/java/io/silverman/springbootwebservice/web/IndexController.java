cb50401b947f5de0adcd509de4d14fcafcc6a256
silvermanseoul <silverman0822@gmail.com>
2021-06-25 21:34:42 +0900

    feat: IndexController 추가

--- src/main/java/io/silverman/springbootwebservice/web/IndexController.java
+++ src/main/java/io/silverman/springbootwebservice/web/IndexController.java
@@ -1,2 +1,13 @@
-package io.silverman.springbootwebservice.web;public class IndexController {
+package io.silverman.springbootwebservice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
