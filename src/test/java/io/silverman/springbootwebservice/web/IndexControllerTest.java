352ea91d5b654e9e277d5173ecdc50afd8a70adb
silvermanseoul <silverman0822@gmail.com>
2021-06-25 21:40:36 +0900

    test: IndexControllerTest 추가

--- src/test/java/io/silverman/springbootwebservice/web/IndexControllerTest.java
+++ src/test/java/io/silverman/springbootwebservice/web/IndexControllerTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.silverman.springbootwebservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class IndexControllerTest {
-  
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void 메인페이지() {
+        // When
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // Then
+        assertTrue(body.contains("스프링 부트 웹 서비스"));
+    }
 }
